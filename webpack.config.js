6e18e92f921d59688ded0d30050e01a9c3c2d666
魏天尧 <tywei90@gmail.com>
2017-07-09 17:13:27 +0800

    1、 解决webpack不能实时监测代码的问题 2、修改底部栏当高度不够的时候不在屏幕底部的问题

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
 
     devServer: {
         contentBase: './src',  //本地服务器所加载的页面所在的目录
-        port: 8888,
+        port: 8080,
         colors: true,  //终端中输出结果为彩色
         historyApiFallback: true,  //不跳转
         inline: true  //实时刷新
